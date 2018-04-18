2c94e9505cd3fc414c3e288a9325e186f35907e7
HaoJiancheng <HaoJianchang@qq.com>
2018-04-18 18:11:12 +0800

    //修改首页公告轮播栏

--- app/src/main/java/com/yixingjjinrong/yixinjinrongapp/xiangmuyemian/shouye/Shouye.java
+++ app/src/main/java/com/yixingjjinrong/yixinjinrongapp/xiangmuyemian/shouye/Shouye.java
@@ -7,18 +7,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.yixingjjinrong.yixinjinrongapp.R;
-import com.yixingjjinrong.yixinjinrongapp.xiangmuyemian.shouye.myView.MarqueeTextView;
-import com.yixingjjinrong.yixinjinrongapp.xiangmuyemian.shouye.myView.MarqueeTextViewClickListener;
+import com.yixingjjinrong.yixinjinrongapp.xiangmuyemian.shouye.myView.NoticeView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Shouye extends Fragment {
 
     //公告栏
-    private MarqueeTextView marqueeTv;
-    private String[] textArrays = new String[]{"一月又一月晕晕晕晕晕晕晕晕晕晕晕晕", "this is content No.2", "this is content No.3"};
-
+    private NoticeView noticeView,noticeTime;
+   
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -31,12 +31,21 @@ public class Shouye extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         //公告栏
-        marqueeTv = getActivity().findViewById(R.id.marqueeTv);
-        marqueeTv.setTextArraysAndClickListener(textArrays, new MarqueeTextViewClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "点击了公告", Toast.LENGTH_SHORT).show();
-            }
-        });
+        noticeView=getActivity().findViewById(R.id.notice_view);
+        noticeTime=getActivity().findViewById(R.id.noticeTime);
+        List<String> notices = new ArrayList<>();
+        notices.add("大促销下单拆福袋，亿万新年红包随便拿");
+        notices.add("家电五折团，抢十亿无门槛现金红包dddddd");
+        notices.add("星球大战剃须刀首发送200元代金券ddddd");
+        noticeView.addNotice(notices);
+        noticeView.startFlipping();
+        //时间
+        List<String> notTime=new ArrayList<>();
+        notTime.add("2018-09-25");
+        notTime.add("2018-09-25");
+        notTime.add("2018-09-25");
+        noticeTime.addNotice(notTime);
+        noticeTime.startFlipping();
+        
     }
 }
--- app/src/main/java/com/yixingjjinrong/yixinjinrongapp/xiangmuyemian/shouye/myView/MarqueeTextViewClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.yixingjjinrong.yixinjinrongapp.xiangmuyemian.shouye.myView;
-
-import android.view.View;
-
-public interface MarqueeTextViewClickListener extends View.OnClickListener{
-    void onClick(View view);
-}

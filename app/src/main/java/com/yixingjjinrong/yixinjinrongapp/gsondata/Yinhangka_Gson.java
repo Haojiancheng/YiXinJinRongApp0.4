60ea5f5e0a8b2a68486460169e7bb8be5dc9ef34
HaoJiancheng <HaoJiancheng@qq.com>
2018-06-27 17:22:02 +0800

    //实名认证，开通银行存管，风险评测，充值

---
+++ app/src/main/java/com/yixingjjinrong/yixinjinrongapp/gsondata/Yinhangka_Gson.java
@@ -0,0 +1,78 @@
+package com.yixingjjinrong.yixinjinrongapp.gsondata;
+
+public class Yinhangka_Gson {
+
+    /**
+     * message : 成功了
+     * bankImage : images/bank1.png
+     * result : {"userId":11208}
+     * state : success
+     * bankName : 中国工商银行
+     * msg :
+     * cardNum : 6222 **** **** 9005
+     */
+
+    private String message;
+    private String bankImage;
+    private String result;
+    private String state;
+    private String bankName;
+    private String msg;
+    private String cardNum;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getBankImage() {
+        return bankImage;
+    }
+
+    public void setBankImage(String bankImage) {
+        this.bankImage = bankImage;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getCardNum() {
+        return cardNum;
+    }
+
+    public void setCardNum(String cardNum) {
+        this.cardNum = cardNum;
+    }
+}

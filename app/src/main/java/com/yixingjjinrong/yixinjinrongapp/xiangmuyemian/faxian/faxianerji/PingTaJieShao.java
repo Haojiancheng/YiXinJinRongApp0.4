2f31f35c06e4f9d3bc972d2178c3c7331a16a41c
HaoJiancheng <HaoJiancheng@qq.com>
2018-08-20 18:27:44 +0800

    //信息披露优化

--- app/src/main/java/com/yixingjjinrong/yixinjinrongapp/xiangmuyemian/faxian/faxianerji/PingTaJieShao.java
+++ app/src/main/java/com/yixingjjinrong/yixinjinrongapp/xiangmuyemian/faxian/faxianerji/PingTaJieShao.java
@@ -1,17 +1,22 @@
 package com.yixingjjinrong.yixinjinrongapp.xiangmuyemian.faxian.faxianerji;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.gyf.barlibrary.ImmersionBar;
 import com.yixingjjinrong.yixinjinrongapp.R;
 import com.yixingjjinrong.yixinjinrongapp.application.AndroidWorkaround;
+import com.yixingjjinrong.yixinjinrongapp.wode.dengruzuce.ZhuCe_PaGa;
+import com.yixingjjinrong.yixinjinrongapp.wode.h5.ZhuCeH5;
 import com.zhy.autolayout.AutoLayoutActivity;
 
 public class PingTaJieShao extends AutoLayoutActivity {
     private ImageView ptjs_fh;
+    private TextView zcxy_h5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +29,21 @@ public class PingTaJieShao extends AutoLayoutActivity {
                 .transparentBar()
                 .fullScreen(false)
                 .init();
-        ptjs_fh=findViewById(R.id.ptjs_fh);
+        ptjs_fh = findViewById(R.id.ptjs_fh);
+        zcxy_h5 = findViewById(R.id.zcxy_h5);
         ptjs_fh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+        zcxy_h5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent zhuce_page = new Intent(PingTaJieShao.this, ZhuCeH5.class);
+                startActivity(zhuce_page);
+            }
+        });
+
     }
 }
